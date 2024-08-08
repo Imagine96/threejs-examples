6b58c4be9544e95b6332aec9b1e9e6657856ce9b
Imagine96 <ruben.guedes@infrared.city>
2024-08-08 12:19:05 +0200

    drag and drop + orbit controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { DragControls } from 'three/addons/controls/DragControls.js';
+
 
 const COLORS = {
   BLUE: "#4f46e5",
@@ -17,10 +20,10 @@ function App() {
   useEffect(() => {
     if (!hasInit.current && started) {
       centerRef.current = []
-
       const scene = new THREE.Scene();
-      const light = new THREE.PointLight(COLORS.WHITE, 1000, 200)
-      const ambientLight = new THREE.AmbientLight(COLORS.WHITE)
+      const raycaster = new THREE.Raycaster();
+      const ambientLight = new THREE.PointLight(COLORS.WHITE, 1000, 500)
+      ambientLight.position.set(5, 20, 0);
 
       const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
       const renderer = new THREE.WebGLRenderer({
@@ -29,48 +32,55 @@ function App() {
 
       renderer.setSize(window.innerWidth, window.innerHeight);
 
-      camera.position.set(5, 20, 10);
+      camera.position.set(5, 20, 0);
 
       document.getElementById("three")?.appendChild(renderer.domElement);
 
-      /* center sphere */
+      const sphere = makeSphere(COLORS.BLUE)
+      scene.add(sphere)
 
-      const centerSphere = makeSphere(COLORS.WHITE, new THREE.Vector3(0.5, 0.5, 0.5), new THREE.Vector3(0, 0, 0), 0.5)
-      scene.add(centerSphere)
-
-      /* adding blue orbital */
-      const centerOrbitBlue = makeCenter()
-      const outerSphereBlue = makeSphere(COLORS.BLUE, new THREE.Vector3(1, 1, 1))
-
-      centerRef.current?.push(centerOrbitBlue)
-
-      centerOrbitBlue.add(outerSphereBlue)
-      outerSphereBlue.position.set(10, 0, 0)
-      scene.add(centerOrbitBlue)
-
-      /* adding green orbital */
-      const centerOrbitGreen = makeCenter()
-      const outerSphereGreen = makeSphere(COLORS.GREEN, new THREE.Vector3(1, 1, 1))
-
-      centerOrbitGreen.add(outerSphereGreen)
-      outerSphereGreen.position.set(10, 0, 10)
-      scene.add(centerOrbitGreen)
-
-      centerRef.current?.push(centerOrbitGreen)
+      const box = makeBox(COLORS.GREEN, new THREE.Vector3(1, 1, 1), new THREE.Vector3(5, 1, 5))
+      scene.add(box)
 
       /* scene final preparations */
-      camera.lookAt(centerOrbitBlue.position)
-      scene.add(light)
+      const gridHelper = new THREE.GridHelper(50)
+      scene.add(gridHelper)
+
+      const orbitControls = new OrbitControls(camera, renderer.domElement)
+      orbitControls.update()
+
+      const dragControls = new DragControls([sphere, box], camera, renderer.domElement)
+
+      dragControls.addEventListener('dragstart', function (event) {
+        (event.object as THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material, THREE.Object3DEventMap>).material.opacity = 0.5
+      });
+
+      dragControls.addEventListener('dragend', function (event) {
+        orbitControls.enabled = true;
+        (event.object as THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material, THREE.Object3DEventMap>).material.opacity = 1
+      });
+
+      renderer.domElement.addEventListener("pointerdown", (event) => {
+        const x = (event.clientX / window.innerWidth) * 2 - 1;
+        const y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        // Raycast from the camera to the mouse position
+        raycaster.setFromCamera(new THREE.Vector2(x, y), camera);
+
+        // Check for intersections with the draggable elements to disable orbit controls
+        if (raycaster.intersectObjects(dragControls.getObjects()).length) {
+          orbitControls.enabled = false
+        } else {
+          orbitControls.enabled = true
+        }
+      })
+
+      camera.lookAt(new THREE.Vector3(0, 0, 0))
       scene.add(ambientLight)
 
       renderer.setAnimationLoop(() => {
+        orbitControls.update()
         renderer.render(scene, camera)
-        if (centerRef.current) {
-          /* rotating every orbital at different speed */
-          centerRef.current.forEach((node, i) => {
-            node.rotation.y += (0.01 * (i + 1))
-          })
-        }
       });
 
       hasInit.current = true
@@ -106,7 +116,8 @@ function App() {
         ) : (
           <div className='start-screen' >
             <div className='card' >
-              <p>Circular orbit</p>
+              <p>Drag and Drop</p>
+              <p>Orbit controls enabled also!</p>
               <button className='start-button' onClick={onStart} >
                 Start
               </button>
@@ -120,10 +131,6 @@ function App() {
 
 export default App
 
-function makeCenter(): THREE.Object3D {
-  return new THREE.Object3D()
-}
-
 function makeSphere(color: string, size?: THREE.Vector3, initialPosition?: THREE.Vector3, opacity?: number): THREE.Mesh {
   const material = new THREE.MeshStandardMaterial({
     color: color,
@@ -131,6 +138,23 @@ function makeSphere(color: string, size?: THREE.Vector3, initialPosition?: THREE
   })
 
   const geometry = new THREE.SphereGeometry()
+  geometry.scale(size?.x || 1, size?.y || 1, size?.z || 1)
+
+  const mesh = new THREE.Mesh(geometry, material)
+  mesh.position.x = initialPosition?.x || 0
+  mesh.position.y = initialPosition?.y || 0
+  mesh.position.z = initialPosition?.z || 0
+
+  return mesh
+}
+
+function makeBox(color: string, size?: THREE.Vector3, initialPosition?: THREE.Vector3, opacity: number = 1): THREE.Mesh {
+  const material = new THREE.MeshStandardMaterial({
+    color: color,
+    opacity: opacity
+  })
+
+  const geometry = new THREE.BoxGeometry()
   geometry.scale(size?.x || 1, size?.y || 1, size?.z || 1)
 
   const mesh = new THREE.Mesh(geometry, material)
